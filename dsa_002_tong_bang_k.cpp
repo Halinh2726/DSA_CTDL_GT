51c4bdb4f1ffe7c347cbb9895d7384b556656496
Pham Ha Linh <phalinh20072006@gmail.com>
2026-01-13 10:27:34 +0700

    Initial commit

---
+++ dsa_002_tong_bang_k.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,k;
+int sum =0;
+vector<vector<int>> res;
+vector<int> tmp;
+void Try(vector<int> &a, int ind)
+{
+    if(sum > k)
+        return;
+    
+    if(sum == k)
+    {
+        res.push_back(tmp);
+        return;
+    }
+
+    if(ind == n)
+    {
+        return;
+    }
+
+    sum += a[ind];
+    tmp.push_back(a[ind]);
+    Try(a,ind+1);
+
+    sum -= a[ind];
+    tmp.pop_back();
+    Try(a,ind+1);
+}
+
+int main()
+{
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0; i< n; i++)
+    {
+        cin >> a[i];
+    }
+
+    Try(a,0);
+    reverse(res.begin(), res.end());
+    int cnt = 0;
+    for(auto &x: res)
+    {
+        for(auto i: x)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
+        cnt++;
+    }
+    cout << cnt << "\n";
+return 0;
+}
